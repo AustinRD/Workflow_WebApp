b3291a5bccda230e6a5f0872ce1be0b6929c455a
AustinRD <austinrd95@gmail.com>
2020-10-14 15:35:36 -0400

    User and Department creation

--- components/userfunctions/create.php
+++ components/userfunctions/create.php
@@ -16,7 +16,7 @@
 
 <!-- Content Title -->
 <header class="w3-container" style="padding-top:22px">
-    <h5><b><i class="fa fa-search"></i>  Admin Create Tool</b></h5>
+    <h5><b><i class="fa fa-plus"></i>  Admin Create Tool</b></h5>
 </header>
 
 <!-- Action Panel -->
@@ -70,27 +70,23 @@
         $userEmail = mysqli_real_escape_string($db_conn, $_POST['email']);
         $bannerID = mysqli_real_escape_string($db_conn, $_POST['banner']);
         $userType = mysqli_real_escape_string($db_conn, $_POST['type']);
+        $userPass = mysqli_real_escape_string($db_conn, $_POST['pswd']);
 
-        $insertclass = "INSERT INTO f20_course_numbers (dept_code, course_number) VALUES ('$dept', '$class')";
-        $insertclassquery = mysqli_query($db_conn, $insertclass);
+        $insertUser = "INSERT INTO f20_userpass (email, passcode, profile_type, verified, first_time) 
+                            VALUES ('$userEmail', '$userPass', '$userType', 1, 0)";
+        $insertUserQuery = mysqli_query($db_conn, $insertUser);
 
         //Database insert success
         if (mysqli_errno($db_conn) == 0) {
-            echo("<div class='w3-panel w3-margin w3-green'><p>Course Successfully Created.</p></div>");
+            echo("<div class='w3-panel w3-margin w3-green'><p>User Successfully Created.</p></div>");
         } 
         //Database detected duplicate entry
         else if (mysqli_errno($db_conn) == 1062) {  
-            echo("<div class='w3-panel w3-margin w3-red'><p>Failed to Create Course - Duplicate Found.</p></div>");
+            echo("<div class='w3-panel w3-margin w3-red'><p>Failed to Create User - Duplicate Found.</p></div>");
         }
     }
     else if (isset($_POST['workflowCreate'])) {
-        $userName = mysqli_real_escape_string($db_conn, $_POST['name']);
-        $userEmail = mysqli_real_escape_string($db_conn, $_POST['email']);
-        $bannerID = mysqli_real_escape_string($db_conn, $_POST['banner']);
-        $userType = mysqli_real_escape_string($db_conn, $_POST['type']);
-
-        $insertclass = "INSERT INTO f20_course_numbers (dept_code, course_number) VALUES ('$dept', '$class')";
-        $insertclassquery = mysqli_query($db_conn, $insertclass);
+        //Needs Implementation
 
         //Database insert success
         if (mysqli_errno($db_conn) == 0) {
@@ -100,23 +96,31 @@
         else if (mysqli_errno($db_conn) == 1062) {  
             echo("<div class='w3-panel w3-margin w3-red'><p>Failed to Create Course - Duplicate Found.</p></div>");
         }
+        else {
+            echo("<div class='w3-panel w3-margin w3-red'><p>Failed to Create Course.</p></div>");
+
+        }
     }
     else if (isset($_POST['departmentCreate'])) {
-        $userName = mysqli_real_escape_string($db_conn, $_POST['name']);
-        $userEmail = mysqli_real_escape_string($db_conn, $_POST['email']);
-        $bannerID = mysqli_real_escape_string($db_conn, $_POST['banner']);
-        $userType = mysqli_real_escape_string($db_conn, $_POST['type']);
+        $deptname = mysqli_real_escape_string($db_conn, $_POST['deptname']);
+        $deptcode = mysqli_real_escape_string($db_conn, $_POST['deptcode']);
+        $deptemail = mysqli_real_escape_string($db_conn, $_POST['deptemail']);
+        $deptsecemail = mysqli_real_escape_string($db_conn, $_POST['deptsecemail']);
 
-        $insertclass = "INSERT INTO f20_course_numbers (dept_code, course_number) VALUES ('$dept', '$class')";
-        $insertclassquery = mysqli_query($db_conn, $insertclass);
+        $insertDeptSQL = "INSERT INTO f20_academic_dept_info (dept_code, dept_name, dean_email, secretary_email) VALUES ('$deptcode', '$deptname', '$deptemail', '$deptsecemail')";
+        $insertDeptQuery = mysqli_query($db_conn, $insertDeptSQL);
 
         //Database insert success
         if (mysqli_errno($db_conn) == 0) {
-            echo("<div class='w3-panel w3-margin w3-green'><p>Course Successfully Created.</p></div>");
+            echo("<div class='w3-panel w3-margin w3-green'><p>Department Successfully Created.</p></div>");
+            $defaultWorkflow = array(0 => 'Student', 1 => 'Instructor', 2 => 'Employer', 3 => 'Chair', 4 => 'Dean', 5 => 'Records&Registration');
+            $defaultWorkflow = serialize($defaultWorkflow);
+            $insertWorkflowSQL = "INSERT INTO f20_workflow_order(dept_code, workflow) VALUES ('$deptcode','$defaultWorkflow')";
+            $insertWorkflowQuery = mysqli_query($db_conn, $insertWorkflowSQL);
         } 
         //Database detected duplicate entry
         else if (mysqli_errno($db_conn) == 1062) {  
-            echo("<div class='w3-panel w3-margin w3-red'><p>Failed to Create Course - Duplicate Found.</p></div>");
+            echo("<div class='w3-panel w3-margin w3-red'><p>Failed to Create Department - Duplicate Found.</p></div>");
         }
     }
 ?>
@@ -133,9 +137,70 @@
 <!-- Create Department -->
 <div id="departmentForm" class="w3-card-4 w3-padding w3-margin" style="display: none;">
     <h5>Create Department</h5>
-    <form method="post">
-        <input type="text"></input>
-        <button type="submit" name="departmentCreate">Create</button>
+    <form method="POST">
+        <label for="dept_name">Department name</label>
+        <input id="dept_name" name="deptname" type="text" class="w3-input">
+        <br>
+        <label for="dept">Department code</label>
+        <input id="dept" name="deptcode" maxlength='3' type="text" class="w3-input">
+        <br>
+        <label for="deptemail">Department Email</label>
+        <input id="deptemail" name="deptemail" type="email" class="w3-input">
+        <br>
+        <label for="deptsecemail">Department Secretary Email</label>
+        <input id="deptsecemail" name="deptsecemail" type="text" class="w3-input">
+        
+        <p><Strong>Permissions</Strong></p>
+            <p>Instructors:<br>
+                <input name="perm1" id="perm1_0" type="checkbox" class="w3-check">
+                <label for="perm1_0" class="custom-control-label">modify course info</label>
+                <input name="perm1" id="perm1_1" type="checkbox" class="w3-check">
+                <label for="perm1_1" class="custom-control-label">modify project info</label>
+                <input name="perm1" id="perm1_2" type="checkbox" class="w3-check">
+                <label for="perm1_2" class="custom-control-label">modify employer info</label></p>
+            <p>Employers:<br>
+                <input name="perm2" id="perm2_0" type="checkbox" class="w3-check">
+                <label for="perm2_0" class="custom-control-label">modify project info</label>
+                <input name="perm2" id="perm2_1" type="checkbox" class="w3-check">
+                <label for="perm2_1" class="custom-control-label">modify learning objectives</label></p>
+            <p>Department Chairs:<br>
+                <input name="perm3" id="perm3_0" type="checkbox" class="w3-check">
+                <label for="perm3_0" class="custom-control-label">modify course info</label>
+                <input name="perm3" id="perm3_1" type="checkbox" class="w3-check">
+                <label for="perm3_1" class="custom-control-label">modify project info</label>
+                <input name="perm3" id="perm3_2" type="checkbox" class="w3-check">
+                <label for="perm3_2" class="custom-control-label">modify employer info</label>
+                <input name="perm3" id="perm3_3" type="checkbox" class="w3-check">
+                <label for="perm3_3" class="custom-control-label">modify learning objectives</label></p>
+        
+        <p><Strong>Email Settings</Strong></p>
+            <p>Students:<br>
+                <input name="em1" id="em1_0" type="checkbox" class="w3-check">
+                <label for="em1_0" class="custom-control-label">receive email updates</label>
+                <input name="em1" id="em1_1" type="checkbox" class="w3-check">
+                <label for="em1_1" class="custom-control-label">receive reminder emails</label></p>
+            <p>Instructors:<br>
+                <input name="em2" id="em2_0" type="checkbox" class="w3-check">
+                <label for="em2_0" class="custom-control-label">receive email updates</label>
+                <input name="em2" id="em2_1" type="checkbox" class="w3-check">
+                <label for="em2_1" class="custom-control-label">receive rejection emails</label>
+                <input name="em2" id="em2_2" type="checkbox" class="w3-check">
+                <label for="em2_2" class="custom-control-label">receive reminder emails</label></p>
+            <p>Department Chairs:<br>
+                <input name="em3" id="em3_0" type="checkbox" class="w3-check">
+                <label for="em3_0" class="custom-control-label">receive email updates</label>
+                <input name="em3" id="em3_1" type="checkbox" class="w3-check">
+                <label for="em3_1" class="custom-control-label">receive rejection emails</label>
+                <input name="em3" id="em3_2" type="checkbox" class="w3-check">
+                <label for="em3_2" class="custom-control-label">receive reminder emails</label></p>
+            <p>Deans:<br>
+                <input name="em4" id="em4_0" type="checkbox" class="w3-check">
+                <label for="em4_0" class="custom-control-label">recieve email updates</label>
+                <input name="em4" id="em4_1" type="checkbox" class="w3-check">
+                <label for="em4_1" class="custom-control-label">receive rejection emails</label>
+                <input name="em4" id="em4_2" type="checkbox" class="w3-check">
+                <label for="em4_2" class="custom-control-label">receive reminder emails</label></p>
+        <button name="departmentCreate" type="submit" class="w3-button w3-teal">Create Department</button>
     </form>
 </div>
 
@@ -171,10 +236,16 @@
     <form method="post" action="./dashboard.php?content=create">
         <label for="name">Name</label>
         <input id="name" name="name" type="text" class="w3-input" required>
+        <br>
         <label for="email">Email Address</label>
-        <input id="email" name="email" placeholder="@newpaltz.edu" type="text" class="w3-input" required>
+        <input id="email" name="email" placeholder="@newpaltz.edu" type="email" class="w3-input" required>
+        <br>
+        <label for="pswd">Password</label>
+        <input id="pswd" name="pswd" type="password" class="w3-input" required>
+        <br>
         <label for="banner">Banner ID</label>
         <input id="banner" name="banner" type="text" class="w3-input" required>
+        <br>
         <label for="type">User Type</label>
         <select id="type" name="type" class="w3-input" required>
             <option value="admin">Admin</option>
