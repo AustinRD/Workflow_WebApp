89fabc870fdd01f947d52c866f78b8b45d52e108
AustinRD <austinrd95@gmail.com>
2020-10-24 18:13:45 -0400

    started work on workflow intiation/start

--- components/userfunctions/workflows.php
+++ components/userfunctions/workflows.php
@@ -40,7 +40,7 @@
         <div class="w3-clear"><h5>New</h5></div>
     </div>
     </div>
-    <div class="w3-quarter">
+    <div class="w3-quarter" onClick="openForm('startWF')">
     <div class="w3-container w3-teal w3-padding-16 w3-border">
         <div class="w3-left"><i class="fa fa-plus w3-xxxlarge"></i></div>
         <div class="w3-right"><h3>&nbsp;</h3></div>
@@ -146,11 +146,85 @@
     </ul>
 </div>
 
+
+<!-- Start Workflow -->
+<div class="w3-card-4 w3-margin w3-padding" id="startWF" style="display:none;">
+    <h5>Start Workflow</h5>
+
+    <form method="get" action="./dashboard.php">
+        <input type="hidden" name="content" value="workflows">
+        <label for="workflowSelect">Workflow Type:</label>
+        <!-- Populate the Select with the list of available workflows for this user -->
+        <!-- Will be hardcoded until custom workflows are implemented -->
+        <select name="workflowSelect" class="w3-input">
+            <option value="internship">Internship/Fieldwork (General)</option>
+            <option value="transferCred">Transfer Credit Evaluation (Not Implemented)</option>
+        </select>
+        <br>
+        <button class="w3-button w3-teal" type="submit" name="startWorkflow">Start</button>
+    </form>
+
+    <!-- If the user selected and submitted a workflow to start -->
+    <?php
+        if(isset($_GET['startWorkflow'])) {
+            if($_GET['workflowSelect'] == 'internship') {
+    ?>
+                <form method="post" action="./dashboard.php?content=workflows">
+                    <label for="studentEmail">Student's Email</label>
+                    <input type="email" name="studentEmail" class="w3-input">
+                    <label for="course">Course</label>
+                    <input type="text" name="course" class="w3-input">
+                    <label for="semester">Semester</label>
+                    <input type="text" name="semester" class="w3-input">
+                    <label for="gradeMethod">Grade Method</label>
+                    <select name="gradeMethod" class="w3-input">
+                        <option value="">Letter Grades</option>
+                        <option value="">Pass/Fail</option>
+                    </select>
+                    <br>
+                    <button class="w3-button w3-teal" type="submit" name="startInternshipWF">Start</button>
+                </form>
+    <?php
+            }
+        }
+    ?>
+
+<!-- Hardcoded handler for starting the internship application. -->
+<?php
+    if (isset($_POST['startInternship'])) { //handles Application submit button
+        $type = mysqli_real_escape_string($db_conn, $_POST['utype']);
+        $sem = mysqli_real_escape_string($db_conn, $_POST['sem']);
+        $grademode = mysqli_real_escape_string($db_conn, $_POST['gm']);
+        $sem = explode(" ", $sem);
+        $type = explode(" ", $type);
+        $semester = $sem[0];
+        $year = $sem[1];
+        $dept = $type[0];
+        $course = $type[1];
+
+        $fwid = bin2hex(random_bytes(32));  //duplication is unlikely with this one. 1 in 20billion apparently
+        $newappsql = "INSERT INTO f20_application_info(fw_id, dept_code, course_number, student_email, semester, year,grade_mode) VALUES ('$fwid','$dept', '$course','$appemail', '$semester', '$year', '$grademode');"; ///get department code
+        $newutilsql = "INSERT INTO f20_application_util(fw_id, progress, rejected, assigned_to, assigned_when) VALUES ('$fwid', '-1', '0', 'student', 'CURRENT_TIMESTAMP');";
+        $insql = mysqli_query($db_conn, $newappsql);
+        if (mysqli_errno($db_conn) == 0) {
+            $insql = mysqli_query($db_conn, $newutilsql);
+            if (mysqli_errno($db_conn) == 0) {
+                header('Location: ./createapplication.php?applicationsuccess=true');
+            }
+        }
+    }
+?>
+
+
+</div>
+
+
 <script>
 function openForm(formID)
 {
     closeForm('newWF');
     closeForm('activeWF');
+    closeForm('startWF');
     document.getElementById(formID).style.display = "block";
 }
 function closeForm(formID)
